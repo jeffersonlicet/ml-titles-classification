893ba535263a6aa1df613d1a2fb148b36fac8cc8
Jefferson Licet <jeffersonlicet@gmail.com>
2019-09-19 19:55:20 +0000

    Sync smarth

--- ml_index_tokens.py
+++ ml_index_tokens.py
@@ -14,7 +14,7 @@ max_sequence = max(len(l) for l in tokens)
 print(max_sequence)
 #max_sequence = max_sequence * 2
 vocabulary_size = dictionary.shape[0] + 1 # to add undefinedword
-chunks = np.array_split(tokens, 8)
+chunks = np.array_split(tokens, 16)
 dic_list = list(dictionary)
 
 print(tokens[0])
@@ -28,9 +28,9 @@ def _hash(arr):
     hashed_list.append([(dic_list.index(token) + 1) for token in item])
   #return pad_sequences(hashed_list, maxlen=max_sequence, padding='post')
   return hashed_list
-with Pool(8) as p:
+with Pool(16) as p:
   data = p.map(_hash, chunks)
-  hashed_list = np.concatenate([data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7]])
+  hashed_list = np.concatenate([data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7], data[8], data[9], data[10], data[11], data[12], data[13], data[14], data[15]])
   np.save('./output_smart/hashed.npy', hashed_list)
   print(hashed_list.shape)
   print(hashed_list[0])
